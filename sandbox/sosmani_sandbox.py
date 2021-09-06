c7301d877f53f9924e550e0fa4bfdc368023c409
Soheb Osmani <78623027+sosmani1@users.noreply.github.com>
2021-09-06 18:36:32 -0400

    Create sosmani_sandbox.py

---
+++ sandbox/sosmani_sandbox.py
@@ -0,0 +1,9 @@
+#Collect input numbers
+a = input('Enter first number: ')
+b = input('Enter second number: ')
+
+#Add the two input numbers
+sum = float(a) + float(b)
+
+#Display the sum of the two numbers
+print('The sum of {0} and {1} is {2}'.format(a, b, sum))
