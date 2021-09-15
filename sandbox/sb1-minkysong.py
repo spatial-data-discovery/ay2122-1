3a3b6e94c24ef6a85f31cda1db2938d2af6ff5a6
minkysong <minkyongsong@Minkyongs-MacBook-Pro.local>
2021-09-14 22:06:05 -0400

    add file for sandbox

---
+++ sandbox/sb1-minkysong.py
@@ -0,0 +1,14 @@
+def sumNum():
+    ### Option A
+
+    # Get input for the two integer
+    a = int(input("Enter the first integer: "))
+    b = int(input("Enter the second integer: "))
+
+    # Print the sum of the two integer 
+    print("The sum of", a, "and", b, "is", a + b)
+
+# Start the program
+if __name__ == '__main__':
+    sumNum()
+
